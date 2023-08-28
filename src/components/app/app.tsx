6eca25cc31ff93bebb18fc33b764ec9172753067
Kekcik <i@batomunkuevv.ru>
2023-08-28 11:43:26 +0900

    Initial app

---
+++ src/components/app/app.tsx
@@ -0,0 +1,10 @@
+import { SiteHeader } from "../site-header";
+
+export const App = () => {
+    return (
+        <div className="wrapper">
+            <SiteHeader />
+            <main className="page"></main>
+        </div>
+    );
+};
